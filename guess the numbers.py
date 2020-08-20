1b0119aff11f82984a948b48f02e6a6983018bab
Lokesh Joshi <69955726+lokeshjoshi01@users.noreply.github.com>
2020-08-20 13:37:12 +0530

    Add files via upload

---
+++ guess the numbers.py
@@ -0,0 +1,50 @@
+n = 7
+inp = int(input("guess the number"))
+if inp>7:
+    print("you are bigger")
+elif inp<7:
+    print("you are smaller")
+else:
+    print("you are win the games")
+print("4 guesses left")
+
+
+n = 7
+inp = int(input("guess the number"))
+if inp>7:
+    print("you are bigger")
+elif inp<7:
+    print("you are smaller")
+else:
+    print("you are win the games")
+print("3 guesses left")
+
+n = 7
+inp = int(input("guess the number"))
+if inp>7:
+    print("you are bigger")
+elif inp<7:
+    print("you are smaller")
+else:
+    print("you are win the games")
+print("2 guesses left")
+
+n = 7
+inp = int(input("guess the number"))
+if inp>7:
+    print("you are bigger")
+elif inp<7:
+    print("you are smaller")
+else:
+    print("you are win the games")
+print("1 guesses left")
+
+n = 7
+inp = int(input("guess the number"))
+if inp>7:
+    print("you are bigger")
+elif inp<7:
+    print("you are smaller")
+else:
+    print("you are win the games")
+print("sorry your guesses are finished")
